1d877f88b47c975f1c4c6149af0cef0aaf950aa4
SafaMarley <safaktrcgl@gmail.com>
2021-08-11 15:53:57 +0300

    episode 1 done

---
+++ FPSTutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private PlayerMotor motor;
+    
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float zMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMovement;
+        Vector3 moveVertical = transform.forward * zMovement;
+
+        Vector3 movingVector = (moveHorizontal + moveVertical).normalized * speed;
+        //motor.move(movingVector);
+    }
+}
