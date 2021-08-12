68aa8a58e1789690adbbd45c0a503ec6739676d4
SafaMarley <safaktrcgl@gmail.com>
2021-08-12 17:38:09 +0300

    episode3&4 done

---
+++ FPSTutorial/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField] private Behaviour[] componentsToDisable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            if (Camera.main != null)
+            {
+                Camera.main.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (Camera.main != null)
+        {
+            Camera.main.gameObject.SetActive(true);
+        }
+    }
+}
